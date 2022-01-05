b7fda605ae8ba583dd80b3353878d5cdcbfe29d8
Fluffy9 <Fluffy9@users.noreply.github.com>
2022-01-04 19:48:04 -0500

    Delete unused interface to save gas

--- babysitter.sol
+++ babysitter.sol
@@ -3,11 +3,6 @@ pragma solidity ^0.8.10;
 
 // Interface for our erc20 token
 interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address tokenOwner)
-        external
-        view
-        returns (uint256 balance);
     function allowance(address tokenOwner, address spender)
         external
         view
@@ -23,68 +18,13 @@ interface IERC20 {
         address to,
         uint256 tokens
     ) external returns (bool success);
-    function mint(address to, uint256 amount) external;
-    function burn(uint256 amount) external;
-    function burnFrom(address account, uint256 amount) external;
 }
 interface ICudlFinance {
-  function MUSE_DAO (  ) external view returns ( address );
-  function MUSE_DEVS (  ) external view returns ( address );
-  function OPERATOR_ROLE (  ) external view returns ( bytes32 );
-  function addOperator ( address _address, bool _isAllowed ) external;
-  function addScore ( uint256 petId, uint256 amount ) external;
-  function addTOD ( uint256 petId, uint256 duration ) external;
-  function burnScore ( uint256 petId, uint256 amount ) external;
-  function burnTod ( uint256 petId, uint256 duration ) external;
   function buyAccesory ( uint256 nftId, uint256 itemId ) external;
-  function careTaker ( uint256, address ) external view returns ( address );
-  function changeEarners ( address _newAddress, address _dao ) external;
-  function changeToken ( address newToken ) external;
-  function claimEarnings (  ) external;
   function claimMiningRewards ( uint256 nftId ) external;
-  function claimMultiple ( uint256[] calldata ids ) external;
-  function createItem ( string calldata name, uint256 price, uint256 points, uint256 timeExtension ) external;
-  function cudlPets (  ) external view returns ( address );
-  function editCurves ( uint256 _la, uint256 _lb, uint256 _ra, uint256 _rb ) external;
-  function editItem ( uint256 _id, uint256 _price, uint256 _points, string calldata _name, uint256 _timeExtension ) external;
-  function fatality ( uint256 _deadId, uint256 _tokenId ) external;
-  function feedMultiple ( uint256[] calldata ids, uint256[] calldata itemIds ) external;
-  function feesEarned (  ) external view returns ( uint256 );
-  function getCareTaker ( uint256 _tokenId, address _owner ) external view returns ( address );
   function getPetInfo ( uint256 _nftId ) external view returns ( uint256 _pet, bool _isStarving, uint256 _score, uint256 _level, uint256 _expectedReward, uint256 _timeUntilStarving, uint256 _lastTimeMined, uint256 _timepetBorn, address _owner, address _token, uint256 _tokenId, bool _isAlive );
   function getRewards ( uint256 tokenId ) external view returns ( uint256 );
-  function giveLife ( address nft, uint256 _id ) external;
-  function giveLifePrice (  ) external view returns ( uint256 );
-  function isNftInTheGame ( address, uint256 ) external view returns ( bool );
-  function isOperator ( address ) external view returns ( bool );
-  function isPetSafe ( uint256 _nftId ) external view returns ( bool );
-  function itemName ( uint256 ) external view returns ( string memory );
-  function itemPoints ( uint256 ) external view returns ( uint256 );
   function itemPrice ( uint256 ) external view returns ( uint256 );
-  function itemTimeExtension ( uint256 ) external view returns ( uint256 );
-  function la (  ) external view returns ( uint256 );
-  function lastBonker (  ) external view returns ( address );
-  function lastTimeMined ( uint256 ) external view returns ( uint256 );
-  function lb (  ) external view returns ( uint256 );
-  function level ( uint256 tokenId ) external view returns ( uint256 );
-  function nftToId ( address, uint256 ) external view returns ( uint256 );
-  function onERC721Received ( address, address, uint256, bytes calldata ) external returns ( bytes4 );
-  function owner (  ) external view returns ( address );
-  function petDead ( uint256 ) external view returns ( bool );
-  function petDetails ( uint256 ) external view returns ( address nft, uint256 id );
-  function petScore ( uint256 ) external view returns ( uint256 );
-  function ra (  ) external view returns ( uint256 );
-  function rb (  ) external view returns ( uint256 );
-  function renounceOwnership (  ) external;
-  function setCareTaker ( uint256 _tokenId, address _careTaker, bool clearCareTaker ) external;
-  function setGiveLifePrice ( uint256 _price ) external;
-  function setPets ( address _pets ) external;
-  function setSupported ( address _nft, bool isSupported ) external;
-  function supportedNfts ( address ) external view returns ( bool );
-  function timePetBorn ( uint256 ) external view returns ( uint256 );
-  function timeUntilStarving ( uint256 ) external view returns ( uint256 );
-  function token (  ) external view returns ( address );
-  function transferOwnership ( address newOwner ) external;
 }
 
 contract BabySitter {
