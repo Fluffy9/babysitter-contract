8a2a3bc31d8fdc2a2fbc027a6f2c33ec1673d4a3
Fluffy9 <Fluffy9@users.noreply.github.com>
2022-01-10 08:53:36 -0500

    Update babysitter.sol

--- babysitter.sol
+++ babysitter.sol
@@ -7,10 +7,14 @@ interface IERC20 {
         external
         view
         returns (uint256 remaining);
-    function transfer(address to, uint256 tokens)
+    function approve(address spender, uint256 tokens)
         external
         returns (bool success);
-    function approve(address spender, uint256 tokens)
+    function balanceOf(address tokenOwner)
+        external
+        view
+    returns (uint256 balance);
+    function transfer(address to, uint256 tokens)
         external
         returns (bool success);
     function transferFrom(
@@ -23,7 +27,6 @@ interface ICudlFinance {
   function buyAccesory ( uint256 nftId, uint256 itemId ) external;
   function claimMiningRewards ( uint256 nftId ) external;
   function getPetInfo ( uint256 _nftId ) external view returns ( uint256 _pet, bool _isStarving, uint256 _score, uint256 _level, uint256 _expectedReward, uint256 _timeUntilStarving, uint256 _lastTimeMined, uint256 _timepetBorn, address _owner, address _token, uint256 _tokenId, bool _isAlive );
-  function getRewards ( uint256 tokenId ) external view returns ( uint256 );
   function itemPrice ( uint256 ) external view returns ( uint256 );
 }
 
@@ -31,8 +34,9 @@ contract BabySitter {
     address public owner;
     uint256 public percentage = 2000;
     mapping (address => uint) public pendingRewards;
-    IERC20 Cudl = IERC20(0x72C546FFABa89b269C4290698D8f106f05e090Fe);
+    IERC20 Cudl = IERC20(0x0f4676178b5c53Ae0a655f1B19A96387E4b8B5f2);
     ICudlFinance CudlFinance = ICudlFinance(0x58b1422b21d58Ae6073ba7B28feE62F704Fc2539);
+    uint256 maxUint256 = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
     constructor() {
         owner = msg.sender;
     }
@@ -50,31 +54,6 @@ contract BabySitter {
     function newPercentage(uint256 _percentage) public onlyOwner {
         percentage = _percentage;
     }
-    // Internal function to claim cudl
-    function Claim(uint256 id) internal returns(uint256){
-        CudlFinance.claimMiningRewards(id);
-        return CudlFinance.getRewards(id);
-    }
-    // Determines whether or not we can feed the pet a particular type of food (or at all)
-    // Pet must be alive
-    // Pet must have a reward amount greater than the food price OR an allowance from the parent that covers the difference
-    function CanFeed(uint256 id, uint256 food) view public returns(bool){
-        (, , , , , , , , address _parent, , , bool _isAlive) = CudlFinance.getPetInfo(id);
-        //pet must be alive
-        if(!_isAlive){ return false; }
-        uint256 reward = CudlFinance.getRewards(id);
-        //rewards must be greater than food price, or the owner is funding it
-        uint256 price = CudlFinance.itemPrice(food);
-        if(reward > price) {
-            return true;
-        }
-        uint256 allowance = Cudl.allowance(_parent, address(this));
-        if(allowance + reward > price){
-            return true;
-        }
-        return false;
-
-    }
     // Feeds and claims cudl from multiple pets
     function FeedMultiple(uint256[] calldata ids, uint256[] calldata food) external {
         for(uint256 i = 0; i < ids.length; i++){
@@ -83,21 +62,21 @@ contract BabySitter {
     }
     // Feeds and claims a singe pet for internal use
     function Feed(uint256 id, uint256 food) internal {
-        require(CanFeed(id, food));
-        uint256 reward = Claim(id);
+        (, , , , , , , , address _parent, , ,) = CudlFinance.getPetInfo(id);
         uint256 price = CudlFinance.itemPrice(food);
         // Give maximum approval to the game contract so that we can always buy food. 
         if(Cudl.allowance(address(this), 0x58b1422b21d58Ae6073ba7B28feE62F704Fc2539) < price){
-            Cudl.approve(0x58b1422b21d58Ae6073ba7B28feE62F704Fc2539, type(uint256).max);
+            Cudl.approve(0x58b1422b21d58Ae6073ba7B28feE62F704Fc2539, maxUint256);
         }
-        (, , , , , , , , address _parent, , ,) = CudlFinance.getPetInfo(id);
+        CudlFinance.buyAccesory(id,food);
+        uint256 before = Cudl.balanceOf(address(this));
+        CudlFinance.claimMiningRewards(id);
+        uint256 reward = Cudl.balanceOf(address(this)) - before;
         if(reward > price){
-            CudlFinance.buyAccesory(id,food);
             Distribute(_parent, reward-price);
             return;
         }
         Cudl.transferFrom(_parent, address(this), price-reward);
-        CudlFinance.buyAccesory(id,food);
     }
     // Distributes the cudl reward among parties to claim later
     function Distribute(address _parent, uint256 reward) private {
@@ -109,6 +88,7 @@ contract BabySitter {
     // Allows the parent to claim their accumulated share of cudl
     function Claim(address _parent) external {
         require(_parent != address(this));
+        require(_parent == msg.sender);
         uint256 amount = pendingRewards[_parent];
         pendingRewards[_parent] = 0;
         Cudl.transfer(_parent, amount);
